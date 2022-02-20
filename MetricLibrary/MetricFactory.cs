56f547de61ff83b672fde84003c9698e6d251d59
Kriistoffer <kjohansson93@hotmail.se>
2022-02-20 14:05:54 +0100

    Started the metric factory

---
+++ MetricLibrary/MetricFactory.cs
@@ -0,0 +1,25 @@
+﻿
+using MetricLibrary.Enums;
+using MetricLibrary.Interfaces;
+
+namespace MetricLibrary
+{
+    public class MetricFactory
+    {
+        public static IMetric Convert(ImperialTypes type)
+        {
+            switch (type)
+            {
+                case ImperialTypes.Inches:
+                    return new Inches();
+                    break;
+                case ImperialTypes.Foot:
+                    return new Foot();
+                    break;
+                default:
+                    return null;
+                    break;
+            }
+        }
+    }
+}
