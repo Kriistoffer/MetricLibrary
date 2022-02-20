5c93adaea477964ffdf5ff333f364741553d337c
Kriistoffer <kjohansson93@hotmail.se>
2022-02-20 14:06:08 +0100

    Created the metric interface

---
+++ MetricLibrary/Interfaces/IMetric.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricLibrary.Interfaces
+{
+    public interface IMetric
+    {
+    }
+}
